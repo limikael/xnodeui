38c8b9dea15a13c2c16c4c2f1277c1d5564ff5be
Mikael Lindqvist <li.mikael@gmail.com>
2014-11-19 11:44:01 +0100

    initial

---
+++ src/xnodeui.js
@@ -0,0 +1,11 @@
+var xnode = require("xnode");
+var inherits = require("inherits");
+var xnodeui = {};
+
+xnodeui.Button = function() {
+
+}
+
+inherits(Button, xnode.Div);
+
+module.exports = xnodeui;
